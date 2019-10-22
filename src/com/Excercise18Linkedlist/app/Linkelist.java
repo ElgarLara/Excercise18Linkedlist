ac9e22c203f747c8d3ae25eecd791014be57882c
akain88 <larahaze88@gmail.com>
2019-10-22 12:50:10 -0500

    examples of the basic lists

---
+++ src/com/Excercise18Linkedlist/app/Linkelist.java
@@ -0,0 +1,22 @@
+package com.Excercise18Linkedlist.app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+public class Linkelist {
+
+	public static void main(String[] args) {
+		// 
+		LinkedList<Byte> miListita=new LinkedList<Byte>();
+		ArrayList<Short> miListita2= new ArrayList<Short>();
+		Stack<Integer> miPillita=new Stack<Integer>();
+		PriorityQueue<Double> miColita=new PriorityQueue<Double>();
+		HashMap<Integer, String> miDiccionaritu =new HashMap<Integer, String>();
+		
+
+	}
+
+}
